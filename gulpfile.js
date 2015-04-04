8005a211e1ddd4a987fdc78c2e20977eaf8d37c1
Joshua Jenkins <josh@joshuajenkins.com>
2015-04-03 21:29:48 -0700

    Improve serving script

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    minifyCSS = require('gulp-minify-css');
+    minifyCSS = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    webserver = require('gulp-webserver');
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/*.scss')
   .pipe(sass())
   .pipe(minifyCSS({'keepSpecialComments': 0}))
-  .pipe(gulp.dest('dist/css/'));
+  .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('default', function() {
+gulp.task('watch', function() {
   gulp.watch('app/scss/*.scss', ['sass']);
 });
+
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+      port: 8080
+    }));
+});
+
+gulp.task('default', ['sass', 'watch', 'webserver']);
